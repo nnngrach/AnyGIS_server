3869808411c5a06a2e9da95614de1d0cc6400555
nnngrach <nnngrach@gmail.com>
2019-02-10 13:13:59 +0300

    git

---
+++ Sources/App/Resouces/CloudinaryApiKey.swift
@@ -0,0 +1,15 @@
+//
+//  CloudinaryApiKey.swift
+//  AnyGIS_ServerPackageDescription
+//
+//  Created by HR_book on 10/02/2019.
+//
+
+import Foundation
+
+struct CloudinaryApiKey {
+    
+    let username = ["username"]
+    let key = ["apikey"]
+    let secret = ["apisecret"]
+}
