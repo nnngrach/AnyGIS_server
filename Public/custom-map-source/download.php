5113e5f89b899f17087669d91071592d3cf28352
nnngrach <nnngrach@gmail.com>
2019-02-20 20:24:11 +0300

    Clean up

--- Public/custom-map-source/download.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$filename = 'galileo-google-maps.ms';
-
-header("Content-type: application/x-download");
-header("Content-Disposition: attachment; filename=$filename");
-readfile($filename);
-
-?>
