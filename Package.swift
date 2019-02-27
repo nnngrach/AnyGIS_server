1a45b5e56cdc4ba64ddbcb763f5d6e5d41f971c8
nnngrach <nnngrach@gmail.com>
2019-02-27 23:13:34 +0300

    test

--- Package.swift
+++ Package.swift
@@ -10,11 +10,13 @@ let package = Package(
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+        
+        .package(url: "https://github.com/yaslab/ZipArchive.swift.git", .upToNextMinor(from: "0.2.0"))
         
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Leaf"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Leaf", "ZipArchive"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
--- Sources/App/Models of Logic/FileGenerator/DiskHandler.swift
+++ Sources/App/Models of Logic/FileGenerator/DiskHandler.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+//import ZipArchive
 
 class DiskHandler {
     
