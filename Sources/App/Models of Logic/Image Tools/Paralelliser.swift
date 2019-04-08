f4e2c8d236c7a3b3a0cd778b8d5b28759f407d88
nnngrach <nnngrach@gmail.com>
2019-04-08 22:17:35 +0300

    Update Paralelliser.swift

--- Sources/App/Models of Logic/Image Tools/Paralelliser.swift
+++ Sources/App/Models of Logic/Image Tools/Paralelliser.swift
@@ -13,7 +13,7 @@ import Foundation
 
 class FreeAccountsParalleliser {
     
-    private let endedCloudinryAccounts : [Int] = [0, 1, 7, 8, 9,
+    private let endedCloudinryAccounts : [Int] = [0, 1, 2, 7, 8, 9,
                                                   11, 12, 13, 14, 15, 19,
                                                   20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
     
