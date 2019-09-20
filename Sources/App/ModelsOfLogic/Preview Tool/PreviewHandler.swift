0118b186d71e676ee283d10337b41873d12a9f76
nnngrach <nnngrach@gmail.com>
2019-09-20 15:07:00 +0300

    fix preview script

--- Sources/App/ModelsOfLogic/Preview Tool/PreviewHandler.swift
+++ Sources/App/ModelsOfLogic/Preview Tool/PreviewHandler.swift
@@ -44,6 +44,7 @@ class PreviewHandler {
                                           maxZoom: maxZoom,
                                           isOverlay: previewRecord.isOverlay)
                 
+                
                 let mapParametersInBase64 = self.convertToBase64(mapParametersInJson)
                 
                 let nakartePrefix = "https://nakarte.me/#m="
@@ -52,6 +53,8 @@ class PreviewHandler {
                 
                 let previewUrl = nakartePrefix + String(previewRecord.previewZoom) + "/" + String(previewRecord.previewLat) + "/" + String(previewRecord.previewLon) + "&l=" + overlayPrefix + "-cs" + mapParametersInBase64!
                 
+                print(mapParametersInJson)
+                print(previewUrl)
     
                 return previewUrl
             }
@@ -66,6 +69,8 @@ class PreviewHandler {
         let processedMapName = replaceIncorrectSymbols(descriptionName)
     
         let anygisMapUrl = SERVER_HOST + dbMapName + "/{x}/{y}/{z}"
+        
+        print(descriptionName)
         
         return """
         {
@@ -94,6 +99,7 @@ class PreviewHandler {
     
     
     private func replaceIncorrectSymbols(_ text: String) -> String {
+        
         var processedText = text
         
         for i in 0 ..< letters.count {
@@ -106,7 +112,8 @@ class PreviewHandler {
     
     
     
-    private let letters = ["а", "б", "в",
+    private let letters = [
+                   "\\","\"","а", "б", "в",
                    "г", "д", "е", "ё", "ж", "з", "и",
                    "й", "к", "л", "м", "н", "о", "п",
                    "р", "с", "т", "у", "ф", "х", "ц",
@@ -116,15 +123,12 @@ class PreviewHandler {
                    "Л", "М", "Н", "О", "П", "Р", "С",
                    "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш",
                    "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я",
-                   "!", "\'", "(", ")", "+", ",", "=",
-                   "@", "`", "_", "-", ".", "§", "«",
-                   "»", "°", "1", "2", "3", "4", "5",
-                   "6", "7", "8", "9", "0", "№", " "]
+                   ]
     
     private let codes = [
-         "\\u0430", "\\u0431", "\\u0432",
+         "\\\\",    "\\\"",    "\\u0430", "\\u0431", "\\u0432",
          "\\u0433", "\\u0434", "\\u0435", "\\u0451", "\\u0436", "\\u0437", "\\u0438",
-         "\\u0439", "\\u043A", "\\u043B", "\\u043C", "\\u043D", "\\uU043E", "\\u043F",
+         "\\u0439", "\\u043A", "\\u043B", "\\u043C", "\\u043D", "\\u043E", "\\u043F",
          "\\u0440", "\\u0441", "\\u0442", "\\u0443", "\\u0444", "\\u0445", "\\u0446",
          "\\u0447", "\\u0448", "\\u0449", "\\u044A", "\\u044B", "\\u044C", "\\u044D",
          "\\u044E", "\\u044F", "\\u0410", "\\u0411", "\\u0412", "\\u0413", "\\u0414",
@@ -132,9 +136,6 @@ class PreviewHandler {
          "\\u041B", "\\u041C", "\\u041D", "\\u041E", "\\u041F", "\\u0420", "\\u0421",
          "\\u0422", "\\u0423", "\\u0424", "\\u0425", "\\u0426", "\\u0427", "\\u0428",
          "\\u0429", "\\u042A", "\\u042B", "\\u042C", "\\u042D", "\\u042E", "\\u042F",
-         "\\u0021", "\\u0027", "\\u0028", "\\u0029", "\\u002B", "\\u002C", "\\u003D",
-         "\\u0040", "\\u0060", "\\u005F", "\\u002D", "\\u002E", "\\u00A7", "\\u00AB",
-         "\\u00BB", "\\u00B0", "\\u0031", "\\u0032", "\\u0033", "\\u0034", "\\u0035",
-         "\\u0036", "\\u0037", "\\u0038", "\\u0039", "\\u0030", "\\u2116", "\\u0020"]
+         ]
 
 }
