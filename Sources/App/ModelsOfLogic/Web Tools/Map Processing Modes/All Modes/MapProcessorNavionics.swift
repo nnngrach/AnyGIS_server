7e6f9b388a3f1edeb3943a09ad366fffa16eeb4d
nnngrach <nnngrach@gmail.com>
2019-10-07 16:57:28 +0300

    Update Navionics auth script

--- Sources/App/ModelsOfLogic/Web Tools/Map Processing Modes/All Modes/MapProcessorNavionics.swift
+++ Sources/App/ModelsOfLogic/Web Tools/Map Processing Modes/All Modes/MapProcessorNavionics.swift
@@ -17,41 +17,26 @@ class MapProcessorNavionics: AbstractMapProcessorSimple {
         let tileUrlBase = urlPatchCreator.calculateTileURL(tileNumbers.x, tileNumbers.y, tileNumbers.z, mapObject)
         
         
-        let checkerURL = mapObject.backgroundServerName + String(Int(NSDate().timeIntervalSince1970))
+        let autherUrl = mapObject.backgroundServerName + String(Int(NSDate().timeIntervalSince1970))
         
-        let headers: HTTPHeaders = ["Origin": "http://webapp.navionics.com",
-                                    "Referer": "http://webapp.navionics.com/"]
+        let headers: HTTPHeaders = ["Origin": "https://webapp.navionics.com",
+                                    "Referer": "https://webapp.navionics.com/",
+                                    "User-Agent": USER_AGENT]
         
+
         
-        let resultResponse = try req
-            .client()
-            .get(checkerURL, headers: headers)
-            .flatMap(to: Response.self) { checkerAnswer in
+        let fullResponce = try req.client()
+            .get(autherUrl, headers: headers)
+            .flatMap(to: Response.self) { authAnswer in
                 
-                let secretCode = "\(checkerAnswer.http.body)"
+                let secretCode = "\(authAnswer.http.body)"
                 
-                let fullURL = tileUrlBase + secretCode
+                let tileUrlWithCode = tileUrlBase + secretCode
                 
-                let connection = HTTPClient.connect(hostname: "backend.navionics.io", connectTimeout: .milliseconds(500), on: req)
-                
-                
-                let result = connection.flatMap(to: Response.self) { client in
-                    
-                    let request = HTTPRequest(method: .GET, url: fullURL, headers: headers, body: "")
-                    let response = client
-                        .send(request)
-                        .map(to: Response.self) { httpRes in
-                            
-                            return Response(http: httpRes, using: req)
-                    }
-                    
-                    return response
-                }
-                
-                return result
+                return try req.client().get(tileUrlWithCode, headers: headers)
         }
         
-        return resultResponse
+        return fullResponce
     }
     
 }
