d54cbb420078d3f5a6ad613cc9ac65ad33792024
nnngrach <nnngrach@gmail.com>
2019-10-07 14:54:04 +0300

    Upd

--- Sources/App/Extantions/GlobalConstants.swift
+++ Sources/App/Extantions/GlobalConstants.swift
@@ -9,4 +9,4 @@ import Foundation
 
 let SITE_HOST = "https://anygis.ru"
 let SERVER_HOST = "https://anygis.ru/api/v1/"
-let USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15"
+let USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
