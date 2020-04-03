5c2dac536e8fbc4b2b33b80863b443c9e7ec38fb
nnngrach <nnngrach@gmail.com>
2020-04-03 22:25:22 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Leaf"]),
         .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App"])
+        //.testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
 
